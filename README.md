ee86059fa9edf4a1dc5961a8de8d2150bece9509
Ariel <ariel.m.little@gmail.com>
2017-02-19 23:24:54 -0500

    basic html fill, starter js object and functions

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # week-4-game
+
+Pseudo Code
+
+1. Random number generated for answer (between 19-120)
+2. 4 crystal numbers generated and assigned to each image (between 1-12)
+3. on click of each crystal, user total increases by the secret crystal number
+4. if user total matches answer, wins increase and game resets
+5. if user total is greater than answer, losses increase and game reset
+6. on game reset new random numbers generated for answer and secret crystal number
+a
---
+++ assets/javascript/game.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+    var game = {
+        answer: 0,
+        userTotal: 0,
+        wins: 0,
+        losses: 0,
+        crystal1: 0,
+        crystal2: 0,
+        crystal3: 0,
+        crystal4: 0,
+
+
+        startGame: function () {
+            // Random number generated for answer (between 19-120)
+            this.answer = [Math.floor(Math.random() * (120 - 19 + 1)) + 19];
+            this.answer = parseInt(this.answer);
+            console.log("Answer: " + this.answer);
+            $("#answer-d").html(this.answer);
+            // 4 crystal numbers generated and assigned to each image (between 1-12)
+            this.crystal1 = [Math.floor(Math.random() * (12 - 1 + 1)) + 1];
+            console.log("crystal1 increment: " + this.crystal1);
+            this.crystal2 = [Math.floor(Math.random() * (12 - 1 + 1)) + 1];
+            console.log("crystal1 increment: " + this.crystal2);
+            this.crystal3 = [Math.floor(Math.random() * (12 - 1 + 1)) + 1];
+            console.log("crystal1 increment: " + this.crystal3);
+            this.crystal4 = [Math.floor(Math.random() * (12 - 1 + 1)) + 1];
+            console.log("crystal1 increment: " + this.crystal4);
+
+            this.collectCrystals();
+        },
+
+        collectCrystals: function () {
+            $("#crystal1-d").click(function (){
+                console.log("you clicked the 1st crystal");
+                game.userTotal += parseInt(game.crystal1);
+                console.log("user total = " + game.userTotal);
+                $("#user-total").html(game.userTotal);
+            });
+            $("#crystal2-d").click(function (){
+                console.log("you clicked the 2nd crystal");
+                game.userTotal += parseInt(game.crystal2);
+                console.log("user total = " + game.userTotal);
+                $("#user-total").html(game.userTotal);
+            });
+            $("#crystal3-d").click(function (){
+                console.log("you clicked the 3rd crystal");
+                game.userTotal += parseInt(game.crystal3);
+                console.log("user total = " + game.userTotal);
+                $("#user-total").html(game.userTotal);
+            });
+            $("#crystal4-d").click(function (){
+                console.log("you clicked the 4th crystal");
+                game.userTotal += parseInt(game.crystal4);
+                console.log("user total = " + game.userTotal);
+                $("#user-total").html(game.userTotal);
+            });
+            this.compare();
+        },
+
+        //this.userTotal and this.answer returning undefined ????
+        compare: function () {
+            if (parseInt(this.userTotal) === parseInt(this.answer)){
+                wins++;
+                $("#wins-d").html(this.wins);
+                console.log("you win");
+            }
+            else if (parseInt(this.userTotal) > parseInt(this.answer)){
+                losses++;
+                $("#losses-d").html(this.losses);
+                console.log("you lose");
+            }
+
+        }
+    };
+
+    game.startGame();
+
+});
